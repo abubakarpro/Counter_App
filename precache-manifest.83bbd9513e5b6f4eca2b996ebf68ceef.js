37f234f056d3b5f28d0211b84430cf0e05d1ae55
Muhammad Abu Bakar <linktobakar@gmail.com>
2019-10-02 01:06:02 +0500

    Updates

---
+++ precache-manifest.83bbd9513e5b6f4eca2b996ebf68ceef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1435f1ad6d58ca1ded9e3f9c5055946",
+    "url": "/Counter_App/index.html"
+  },
+  {
+    "revision": "6f0c705d69b3d2c7b26d",
+    "url": "/Counter_App/static/css/main.c3c20921.chunk.css"
+  },
+  {
+    "revision": "d2d82d68c7df91bb776f",
+    "url": "/Counter_App/static/js/2.e703e8cc.chunk.js"
+  },
+  {
+    "revision": "6f0c705d69b3d2c7b26d",
+    "url": "/Counter_App/static/js/main.4c5fa187.chunk.js"
+  },
+  {
+    "revision": "a062724aa814e0f42a8a",
+    "url": "/Counter_App/static/js/runtime~main.1826334a.js"
+  }
+]);
